d8b8c058ce549ad26105235d2fbbffa66c6a77fc
Ilya <ilyamazein@gmail.com>
2019-05-23 16:12:52 +0300

    Create domain calculation service

---
+++ Domain/services/calculationService.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"github.com/apmath-web/interests/Domain"
+	"github.com/apmath-web/interests/Domain/models"
+)
+
+type CalculationService struct {
+	clientFetcher Domain.ClientFetchInterface
+}
+
+func (cs *CalculationService) Calculate(ids Domain.IdsDomainModelInterface) (Domain.InterestsInterface, error) {
+	var persons []Domain.PersonDomainModelInterface
+
+	var pdm, err = cs.clientFetcher.Fetch(ids.GetClientId())
+	if err != nil {
+		return nil, err
+	}
+	persons = append(persons, pdm)
+
+	for _, value := range ids.GetCoborrowersIdSlice() {
+		var pdm, err = cs.clientFetcher.Fetch(value)
+		if err != nil {
+			return nil, err
+		}
+		persons = append(persons, pdm)
+	}
+
+	var percent = models.Calculate(persons)
+	var interests = models.GenInterestsDomainModel(percent)
+
+	return interests, nil
+}
+
+func (cs *CalculationService) GenCalculationService(clientFetch Domain.ClientFetchInterface) {
+	cs.clientFetcher = clientFetch
+}
