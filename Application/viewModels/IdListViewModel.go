6771f5452f8f7aff831c708a404eef63feaea9a2
Ilya <ilyamazein@gmail.com>
2019-05-23 23:40:25 +0300

    Create viewModel

---
+++ Application/viewModels/IdListViewModel.go
@@ -0,0 +1,50 @@
+package viewModels
+
+import (
+	"encoding/json"
+	"github.com/apmath-web/expenses/Application/Validation"
+	"github.com/apmath-web/expenses/Domain"
+)
+
+type IdsViewModel struct {
+	CoborrowersIdSlice []int `json:"coBorrowers"`
+	validation Validation.Validation
+}
+
+
+func (idsViewModel *IdsViewModel) GetCoborrowersIdSlice() []int {
+	return idsViewModel.CoborrowersIdSlice
+}
+
+
+func (idsViewModel *IdsViewModel) validateCoBorrowerIdSlice() bool {
+	for _, id := range idsViewModel.CoborrowersIdSlice {
+		if id < 0 {
+			idsViewModel.validation.AddMessage(Validation.GenMessage("coBorrowers", "Is negative"))
+			return false
+		}
+	}
+	return true
+}
+
+func (idsViewModel *IdsViewModel) Validate() bool {
+	if idsViewModel.validateCoBorrowerIdSlice() {
+		return true
+	}
+	return false
+}
+
+
+func (idsViewModel *IdsViewModel) UnmarshalJSON(b []byte) error {
+	tmpIds := idsViewModel.CoborrowersIdSlice
+	err := json.Unmarshal(b, &tmpIds)
+	if err := json.Unmarshal(b, &tmpIds); err != nil {
+		return err
+	}
+	idsViewModel.CoborrowersIdSlice = tmpIds
+	return err
+}
+
+func (idsViewModel *IdsViewModel) GetValidation() Domain.ValidationInterface {
+	return &idsViewModel.validation
+}
---
+++ Application/viewModels/InterestsViewModel.go
@@ -0,0 +1,30 @@
+package viewModels
+
+import (
+	"encoding/json"
+	"github.com/apmath-web/interests/Application/Validation"
+	"github.com/apmath-web/interests/Domain"
+)
+
+type JsonInterests struct {
+	interest float64 `json:"interest"`
+}
+
+type InterestsViewModel struct {
+	JsonInterests
+	validation Validation.Validation
+}
+
+func (interestsViewModel *InterestsViewModel) GetInterest() float64 {
+	return interestsViewModel.interest
+}
+
+func (interestsViewModel *InterestsViewModel) MarshalJSON() (b []byte, e error) {
+	return json.Marshal(map[string]interface{}{
+		"interest": interestsViewModel.interest,
+	})
+}
+
+func (interestsViewModel *InterestsViewModel) Hydrate(interestsInerface Domain.InterestsInterface) {
+	interestsViewModel.interest = interestsInerface.GetInterest()
+}
