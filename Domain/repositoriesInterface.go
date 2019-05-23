1b6e0e2e7f798f7c6e5fa764da141604203ae640
Ilya <ilyamazein@gmail.com>
2019-05-23 16:07:09 +0300

    Update repositoriesInterface.go

--- Domain/repositoriesInterface.go
+++ Domain/repositoriesInterface.go
@@ -4,3 +4,7 @@ type repositoryInterface interface {
 	GetModel(id int) HelloWorldApplicationModel
 	PutModel(model HelloWorldApplicationModel) int
 }
+
+type ClientFetchInterface interface {
+	Fetch(id int) (PersonDomainModelInterface, error)
+}
