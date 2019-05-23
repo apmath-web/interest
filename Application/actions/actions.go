b5ee181b0cd61a23a87afb13c57d8ac881a7d8a2
Ilya <ilyamazein@gmail.com>
2019-05-24 00:49:45 +0300

    Reinstate template parts

--- Application/actions/actions.go
+++ Application/actions/actions.go
@@ -1,1 +1,25 @@
 package actions
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/apmath-web/interests/Application/viewModels"
+	"github.com/apmath-web/interests/Infrastructure/repositories"
+	"github.com/gin-gonic/gin"
+)
+
+func HelloWorldHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, viewModels.GenHelloWorldViewModel("Wrong id", "400"))
+		return
+	}
+	hw := repositories.Repo.GetModel(id)
+	if hw == nil {
+		c.JSON(http.StatusNotFound, viewModels.GenHelloWorldViewModel("No model", "404"))
+		return
+	}
+	helloWorldViewModel := viewModels.GenHelloWorldViewModel(hw.GetMessage(), "200")
+	c.JSON(http.StatusOK, helloWorldViewModel)
+}
