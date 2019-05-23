a8376d877b5f2246518a85e03ad9c317ea6c8199
Ilya <ilyamazein@gmail.com>
2019-05-24 00:21:33 +0300

    Create infrastructure applicationModels

---
+++ Infrastructure/applicationModels/JobApplicationModel.go
@@ -0,0 +1,14 @@
+package applicationModels
+
+type JobApplicationModel struct {
+	Name string
+	Wage int
+}
+
+func (job *JobApplicationModel) GetName() string {
+	return job.Name
+}
+
+func (job *JobApplicationModel) GetWage() int {
+	return job.Wage
+}
