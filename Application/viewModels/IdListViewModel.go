046ad407e79844acae825db86631b7178a214658
varlogen <martynovpavel8@ya.ru>
2019-05-24 19:31:09 +0300

    changes according to new specification

--- Application/viewModels/IdListViewModel.go
+++ Application/viewModels/IdListViewModel.go
@@ -7,15 +7,50 @@ import (
 )
 
 type IdsViewModel struct {
+	ClientId           int   `json:"clientId"`
 	CoborrowersIdSlice []int `json:"coBorrowers"`
-	validation Validation.Validation
+	validation         Validation.Validation
 }
 
+func (idsvm *IdsViewModel) SetClientId(clientId int) {
+	idsvm.ClientId = clientId
+}
+
+func (idsvm *IdsViewModel) SetCoBorrowersIdSlice(coborrowersIdSlice []int) {
+	idsvm.CoborrowersIdSlice = coborrowersIdSlice
+}
+
+func GenIdsViewModel(clientId int, coborrowersIdSlice []int) *IdsViewModel {
+	idsvm := new(IdsViewModel)
+	idsvm.SetClientId(clientId)
+	idsvm.SetCoBorrowersIdSlice(coborrowersIdSlice)
+	return idsvm
+}
+
+func (idsViewModel *IdsViewModel) GetClientId() int {
+	return idsViewModel.ClientId
+}
 
 func (idsViewModel *IdsViewModel) GetCoborrowersIdSlice() []int {
 	return idsViewModel.CoborrowersIdSlice
 }
 
+func (idsViewModel *IdsViewModel) validateClientId() bool {
+	clientId := idsViewModel.GetClientId()
+	if clientId < 0 {
+		idsViewModel.validation.AddMessage(Validation.GenMessage("clientId", "Is negative"))
+		return false
+	}
+
+	for _, id := range idsViewModel.CoborrowersIdSlice {
+		if clientId == id {
+			idsViewModel.validation.AddMessage(Validation.GenMessage("clientId", "matches the co-borrower id"))
+			return false
+		}
+	}
+
+	return true
+}
 
 func (idsViewModel *IdsViewModel) validateCoBorrowerIdSlice() bool {
 	for _, id := range idsViewModel.CoborrowersIdSlice {
@@ -24,16 +59,27 @@ func (idsViewModel *IdsViewModel) validateCoBorrowerIdSlice() bool {
 			return false
 		}
 	}
+
+	ids := idsViewModel.CoborrowersIdSlice
+	for id1 := 0; id1 < len(ids); id1++ {
+		for id2 := id1; id2 < len(ids); id2++ {
+			if ids[id1] == ids[id2] && id1 != id2 {
+				idsViewModel.validation.AddMessage(Validation.GenMessage("coBorrowers", "Is not unique"))
+				return false
+			}
+		}
+
+	}
+
 	return true
 }
 
 func (idsViewModel *IdsViewModel) Validate() bool {
-	if idsViewModel.validateCoBorrowerIdSlice() {
+	if idsViewModel.validateCoBorrowerIdSlice() && idsViewModel.validateClientId() {
 		return true
 	}
 	return false
 }
-
 
 func (idsViewModel *IdsViewModel) UnmarshalJSON(b []byte) error {
 	tmpIds := idsViewModel.CoborrowersIdSlice
