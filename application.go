143a5ad4b5eb9cb72c6c5f72b256173f84014219
varlogen <martynovpavel8@ya.ru>
2019-05-24 22:21:17 +0300

    comment unexisted object

--- application.go
+++ application.go
@@ -1,15 +1,12 @@
 package main
 
-import (
-	"github.com/apmath-web/interests/Application/routing"
-	"log"
-)
-
 func main() {
-	//repositories.Repo.PutModel(applicationModels.GenHelloWorldApplicationModel("hello_world"))
-	router := routing.GenRouter()
-	err := router.Run("0.0.0.0:8080")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	/*
+		repositories.Repo.PutModel(applicationModels.GenHelloWorldApplicationModel("hello_world"))
+		router := routing.GenRouter()
+		err := router.Run("0.0.0.0:8080")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	*/
 }
