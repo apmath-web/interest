ee00288ad8fa9372d3470ce64de1072a5ec888a8
Ilya <ilyamazein@gmail.com>
2019-05-27 20:31:06 +0300

    Update application.go

--- application.go
+++ application.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"github.com/apmath-web/interests/Application/routing"
-	"github.com/apmath-web/interests/Infrastructure/applicationModels"
-	"github.com/apmath-web/interests/Infrastructure/repositories"
+	"github.com/apmath-web/expenses/Application/routing"
 	"log"
 )
 
 func main() {
-	repositories.Repo.PutModel(applicationModels.GenHelloWorldApplicationModel("hello_world"))
 	router := routing.GenRouter()
 	err := router.Run("0.0.0.0:8080")
 	if err != nil {
