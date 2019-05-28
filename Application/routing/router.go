8a9acfddb05bfa6ed8719e8762f8cfdb22eac491
Ilya <ilyamazein@gmail.com>
2019-05-28 13:09:12 +0300

    Create route

--- Application/routing/router.go
+++ Application/routing/router.go
@@ -1,7 +1,7 @@
 package routing
 
 import (
-	"github.com/apmath-web/template-go/Application/actions"
+	"github.com/apmath-web/interests/Application/actions"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,7 +9,7 @@ func GenRouter() *gin.Engine {
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	{
-		v1.GET("/hello_world/:id", actions.HelloWorldHandler)
+		v1.POST("/:clientId", actions.GetInterests)
 	}
 	return router
 }
