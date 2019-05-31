3e56d93fd5f0609c53b809840cb84e110caa1cf4
varlogen <martynovpavel8@ya.ru>
2019-05-31 12:46:38 +0300

    create clientFetchService

---
+++ Infrastructure/ClientFethService.go
@@ -0,0 +1,48 @@
+package Infrastructure
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/apmath-web/expenses/Domain"
+	"github.com/apmath-web/expenses/Infrastructure/Mapper"
+	"github.com/apmath-web/expenses/Infrastructure/applicationModels"
+	"net/http"
+	"strconv"
+)
+
+type clientFetchService struct {
+	url string
+}
+
+func GenClientFetchService(host string, port string, version string) Domain.ClientFetchInterface {
+	var instantiated *clientFetchService
+	instantiated = &clientFetchService{"http://" + host + ":" + port + "/" + version + "/"}
+	return instantiated
+}
+
+func (clfs *clientFetchService) Fetch(id int) (Domain.PersonDomainModelInterface, error) {
+	resp, err := http.Get(clfs.url + strconv.Itoa(id))
+	if resp == nil {
+		return nil, errors.New("clients service not available")
+	}
+	if err != nil {
+		return nil, err
+	}
+	person := new(applicationModels.PersonApplicationModel)
+
+	if resp.StatusCode == http.StatusOK {
+		dec := json.NewDecoder(resp.Body)
+		err := dec.Decode(&person)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if resp.StatusCode == http.StatusBadRequest {
+		return nil, errors.New("bad request")
+	}
+	if resp.StatusCode == http.StatusNotFound {
+		return nil, errors.New("client not found")
+	}
+	pdm := Mapper.PersonApplicationMapper(*person)
+	return pdm, nil
+}
